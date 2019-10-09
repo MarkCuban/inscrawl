d06a98eded447d487f26c0b61914ed6dbb31eb65
MarkCuban <86317539@qq.com>
2019-10-09 17:33:12 +0800

    update

--- Inscrawl3.py
+++ Inscrawl3.py
@@ -1,10 +1,17 @@
 # -*- coding:utf-8 -*-
 import requests
 import asyncio
+import json
+import collections
+from collections.abc import Awaitable
+from requests import Response
+import signal
 
 from lxml import etree
 
-PAGE_NUM = 0
+PAGE_NUM = 1
+
+WITH_PROXY = False
 
 URLS = [    'https://www.instagram.com/JayChou/',
             #'https://www.instagram.com/b_b_j.j/',
@@ -24,7 +31,8 @@ HEADER = {
     'accept': '*/*',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'mid=XXtloAAEAAFPDIVBwVQCwlLVlXzB; csrftoken=nVAS2jad5L90qVFEYkHArcD6fmU5GUEn; ds_user_id=1285777283; sessionid=1285777283%3AUWUWkFMel6xecU%3A25; shbid=12365; shbts=1570324023.8633752; rur=FRC; urlgen="{\"45.32.251.0\": 20473}:1iHrqf:sYnjYctwHDIoLIb5kAWfWzuQxUE"',
+    #'cookie': 'mid=XXtloAAEAAFPDIVBwVQCwlLVlXzB; csrftoken=nVAS2jad5L90qVFEYkHArcD6fmU5GUEn; ds_user_id=1285777283; sessionid=1285777283%3AUWUWkFMel6xecU%3A25; shbid=12365; shbts=1570324023.8633752; rur=FRC; urlgen="{\"45.32.251.0\": 20473}:1iHrqf:sYnjYctwHDIoLIb5kAWfWzuQxUE"',
+    'cookie':'mid=XXtloAAEAAFPDIVBwVQCwlLVlXzB; csrftoken=nVAS2jad5L90qVFEYkHArcD6fmU5GUEn; shbid=12365; shbts=1568607008.3552365; ds_user_id=1285777283; sessionid=1285777283%3AUWUWkFMel6xecU%3A25; rur=FRC; urlgen="{\"45.63.51.0\": 20473}:1iA9FD:UCJzQ03-A-j9c-PuNt8wjncDJbA"',
     'referer': 'https://www.instagram.com/',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
 }
@@ -36,6 +44,7 @@ PAGE_URL = 'https://www.instagram.com/graphql/query/?query_hash=58b6785bea111c67
 TARGET_SHORTCODE_STR = '{shortcode}'
 SIDECAR_URL = 'https://www.instagram.com/graphql/query/?query_hash=865589822932d1b43dfe312121dd353a&variables=%7B%22shortcode%22%3A%22'+TARGET_SHORTCODE_STR+'%22%2C%22child_comment_count%22%3A3%2C%22fetch_comment_count%22%3A40%2C%22parent_comment_count%22%3A24%2C%22has_threaded_comments%22%3Atrue%7D'
 
+NAME_REFE = '?_nc_ht='
 
 url_list = {}
 
@@ -59,20 +68,6 @@ def open_json(tar):
         pass
 
     return js
-
-def request_url(url):
-    yield 
-
-    print('url {} request start'.format(url))
-    if url is not None:    
-        res = requests.get(url, headers=HEADER, proxies=proxy)
-    
-        if res.status_code == 200:
-            return res.content.decode()
-        else:
-            pass   
-
-    return None  
 
 def parse_html(raw):
         
@@ -229,43 +224,120 @@ def parseJSON(dict_idx, json):
 
     return None
 
-def parse_url(content):
-    yield
-
+def parse_url(content, url):
+    
     print('url {} parse start'.format(url))
     js_data = open_json(content)
 
     if js_data == None:
         js_data = parse_html(content)
 
-    parseJSON(None, js_data)
+    parseJSON(url, js_data)
 
-@asyncio.coroutine
-def request_and_parse(url):
- 
+async def request_url(url):
+    print('url {} request start'.format(url))
+
+    loop = asyncio.get_event_loop()
+    if url is not None:  
+
+        if WITH_PROXY == True: 
+            res = await loop.run_in_executor(None, requests.get, url, HEADER, proxy)
+        else:
+            res = await loop.run_in_executor(None, requests.get, url, HEADER)
+    return res
+'''
+        if res.status_code == 200:
+            return res.content.decode()
+        else:
+            pass
+'''
+    
+   
+async def request_and_parse(url, idx):
+    res = None
     while True:
         try:
-            res_list = yield from request_url(url)
+            res = await request_url(url)
         except Exception as e:
             print(e)
 
-        if res_list is None:
+        if isinstance(res, Response):
+            if res.status_code == 200:
+                parse_url(res.content.decode(), idx)
+            else:
+                print('not 200, try again')
+        else:
             continue
 
-        yield from parse_url(res_list)
-        print('url is parsed over', url)
-        break 
-    url_list.popitem(url)
+        break
+        
+    print('url is parsed over', url)     
+    url_list.pop(url)
+
+def save_img(con, fname):
+
+    with open(fname, 'wb') as f:
+        f.write(con)
+
+
+async def download_single(url):
+    res = None
+
+    fname = url.split(NAME_REFE)[0].split('/')[-1]
+    while True:
+
+        try:
+            res = await request_url(url)
+        except Exception as e:
+            print(e)
+
+        if isinstance(res, Response):
+            if res.status_code == 200:
+                save_img(res.content, fname)
+            else:
+                print('not 200, try again')            
 
 def initial():
+
     for url in URLS:
         url_list[url] = url
+        img_urls[url] = []
+        video_urls[url] = []
+        user_id[url] = []
+        PAGE_IDX[url] = 0    
 
-def crawl():
+def showParseRes():
+
+    global img_urls, video_urls
+
+    print('-------------------- crawl ins result --------------------')
+    
+    for key in img_urls.keys():
+        print('------------------'+key+'------------------')
+        print('Images: '+str(len(img_urls[key]))+' Videos: '+str(len(video_urls[key])))
+        print('-------------------------------------------')             
+
+def my_handler():
+    print('stopping')
+    for task in asyncio.Task.all_tasks():
+        task.cancel()
+
+def write_json_files(src, filename):
+    with open(filename, 'w+') as f:
+        js_str = json.dumps(src, indent=4)
+        f.write(js_str)
+        f.close()   
+
+def url_save():
+    global img_urls, video_urls
+
+    #write_json_files(url_list, 'urls.json')
+    write_json_files(img_urls, 'img_urls.json')
+    write_json_files(video_urls, 'video_urls.json')
+
+def crawl(loop):
 
     initial()
-    loop = asyncio.get_event_loop()
-
     tasks = []
 
     print(url_list)
@@ -275,15 +347,31 @@ def crawl():
             break
 
         for key in url_list.keys():
-            gen = request_and_parse(key)
+            gen = request_and_parse(key, url_list[key])
             tasks.append(gen)
+        
+        try:
+            
+            loop.run_until_complete(asyncio.wait(tasks))
+             
+        except Exception as e:
+            print(e)
 
-        loop.run_until_complete(asyncio.wait(tasks))
+    
+    showParseRes()  
 
-    loop.stop()    
+def url_down():
 
-def main():  
-    crawl()
+
+def main(): 
+
+    loop = asyncio.get_event_loop()
+
+    crawl(loop)
+    url_save()
+    url_down()
+
+    loop.close()
 
 if __name__ == "__main__":
     main()
